2d1258179c19f8c9dd7e7653101dfd5c41c09865
Jon Gray <jgray@spartaglobal.com>
2018-11-22 15:06:23 +0000

    full array lab

--- array_lab/array_lab/spec/array_spec.rb
+++ array_lab/array_lab/spec/array_spec.rb
@@ -5,18 +5,18 @@ describe 'working through various arrays' do
   context 'standard 1 dimensional arrays' do
     dog_breeds = ['jack russell', 'boxer', 'bloodhound', 'greyhound']
 
-    it 'the first entry should equal jack russel' do
-      # Use array methods only
+    it 'the first entry should equal jack russell' do
+      expect(dog_breeds.first).to eq 'jack russell'
     end
 
     it 'the last entry should equal greyhound' do
       # Use array methods only
-      pending
+      expect(dog_breeds.last).to eq 'greyhound'
     end
 
     it 'the second entry should equal boxer' do
       # Use array index only i.e. array[0]
-      pending
+      expect(dog_breeds[1]).to eq 'boxer'
     end
 
   end
@@ -27,17 +27,17 @@ describe 'working through various arrays' do
 
     it 'the first dog name should equal archie' do
       # Use a mix of array methods and indexes
-      pending
+      expect(dog_breed_and_names.first[1]).to eq 'archie'
     end
 
     it 'the second last entry should be a bloodhound called droopy' do
       # Use array index only i.e. array[0]
-      pending
+      expect(dog_breed_and_names[-2]).to eq ['bloodhound', 'droopy']
     end
 
     it 'the second entry should be a boxer' do
       # Use array index only i.e. array[0]
-      pending
+      expect(dog_breed_and_names[1][0]).to eq 'boxer'
     end
 
   end
@@ -46,18 +46,18 @@ describe 'working through various arrays' do
     dog_breed_names_age_and_health = [['jack russell', 'archie',[4, 'healthy']],['boxer', 'bruce',[7,'bad back']],['bloodhound', 'droopy', [6, 'healthy']], ['greyhound', 'streak',[12, 'old dog']]]
 
     it 'the last dog should be called streak and is an old dog' do
-      # Use a mix of array methods and indexes
-      pending
+      expect(dog_breed_names_age_and_health.last[2].last).to eq 'old dog'
     end
 
     it 'the bloodhound should be 6 years old and healthy' do
       # Use a mix of array methods and indexes
-      pending
+      expect(dog_breed_names_age_and_health[2].last).to eq [6, 'healthy']
     end
 
     it 'the first dog should be a jack russell and 4 years old' do
       # Use array index only i.e. array[0]
-      pending
+      expect(dog_breed_names_age_and_health[0][0]).to eq 'jack russell'
+      expect(dog_breed_names_age_and_health[0][2][0]).to eq 4
     end
 
   end
